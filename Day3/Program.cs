17048a2b349d4158b8dd6c188754de0aebedb1eb
ADMIN\bszha <sze19728@spengergasse.at>
2022-12-03 18:39:23 +0100

    Day 3 Completed

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -8,8 +8,8 @@ namespace Day3
     {
         const int A = 65-27;
         const int a = 97-1;
-        static int EvaluateChar(char c) => (int)(c) - ((int)(c) >= 97 ? a : A); 
-        static int score = 0;
+        static int EvaluateChar(char c) => c - (c >= 97 ? a : A); 
+        static int[] score = new int[] { 0, 0 };
         static char GetCommonChar(string a, string b)
         {
             for (int i = 65; i < 124; i++)
@@ -17,23 +17,37 @@ namespace Day3
                     return (char)i;
             return '?';
         }
+        static char GetCommonChar(string[] group)
+        {
+            foreach(char c in group[0])
+                if(group[1].Contains(c)&& group[2].Contains(c))
+                    return c;
+            return '?';
+        }
         static void Main(string[] args)
         {
             var lines = Common.ParseFile(@"input.txt");
+            int count = 0;
             foreach (string rucksack in lines)
             {
+                
                 int compartmentSize = rucksack.Length / 2;
                 var firstCompartment = rucksack.Substring(0, compartmentSize);
                 var secondCompartment = rucksack.Substring(compartmentSize);
                 char common = GetCommonChar(firstCompartment, secondCompartment);
-                score += EvaluateChar(common);
+                score[0] += EvaluateChar(common);
 
                 Console.WriteLine($"rucksack: {rucksack} + {rucksack.Length}\n" +
                     $"firstCompartment: {firstCompartment} + {firstCompartment.Length}\n" +
                     $"secondCompartment: {secondCompartment} + {secondCompartment.Length}\n" +
                     $"common: {common}\n" +
                     $"score: {EvaluateChar(common)}\n" +
-                    $"total: {score}");
+                    $"total: {score[0]} + {score[1]}");
+
+                if (count % 3 == 0)
+                    score[1] += EvaluateChar(GetCommonChar(new string[] { lines[count], lines[count + 1], lines[count + 2] }));
+                
+                count++;
             }
         }
     }

f39144bd855c496007cab83903354f341e1c4b9c
ADMIN\bszha <sze19728@spengergasse.at>
2022-12-03 13:45:31 +0100

    day 1 init

---
+++ Day1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AoC2022;
+namespace Day1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}

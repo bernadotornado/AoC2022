e61cba4c46c464d2d61ffc30ec4b1dc0429f6cf1
ADMIN\bszha <sze19728@spengergasse.at>
2022-12-03 14:13:16 +0100

    Day 1 Completed

--- AoC2022/Common.cs
+++ AoC2022/Common.cs
@@ -1,7 +1,27 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 namespace AoC2022
 {
    public class Common
    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static List<string> ParseFile(string path)
+        {
+            StreamReader sr = new StreamReader(@"..\..\..\"+path);
+            List<string> s = new List<string>();
+            string line;
+
+            while((line = sr.ReadLine()) != null)
+            {
+                s.Add(line);
+            }
+            return s;
+        }
+
    }
 }
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using AoC2022;
 namespace Day1
 {
@@ -6,8 +7,21 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
+            List<int> sums = new List<int>();
+            var lines = Common.ParseFile(@"input.txt");
+            int currentNum = 0;
+            foreach(var item in lines)
+            {
+                if(item == "")
+                {
+                    sums.Add(currentNum);
+                    currentNum = 0;
+                    continue;
+                }
+                currentNum += Convert.ToInt32(item);
+            }
+            sums.Sort();
+            Console.WriteLine($"most calories: {sums[sums.Count-1]}");
         }
     }
 }

ffd0718207f91750d722214f6e048b4fe59cdee6
SZE19728 <SZE19728@spengergasse.at>
2022-12-07 11:11:23 +0100

    test input correct, puzzle input too high

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -18,45 +18,118 @@ namespace Day7
     public class Directory
     {
         public string name;
-        public List<File> filesInDirectory;
-        public List<Directory> subDirectories;
-        public Directory(string name)
+        public int depth;
+        public int dirSize = 0;
+        public int totalSize = 0;
+        public List<File> filesInDirectory = new List<File>();
+        public List<Directory> subDirectories = new List<Directory>();
+        public Directory parent;
+        public Directory(string name, int depth)
         {
             this.name = name;
+            this.depth = depth;
+        }
+
+        public int CountTotalSize()
+        {
+            int x = 0;
+            foreach (var dir in subDirectories)
+            {
+                x += dir.CountTotalSize();
+            }
+
+            totalSize = dirSize + x;
+            return totalSize;
+        }
+        public void CountDirSize()
+        {
+            foreach (var file in filesInDirectory)
+            {
+                dirSize += file.fileSize;
+            }
+        }
+        
+        public void PrintDir (){
+            for (int i = 0; i < depth; i++)
+            {
+                Console.Write("\t");
+            }
+            Console.Write($"Listing Dir: {name}");
+            Console.WriteLine();
+
+            
+            if (filesInDirectory != null)
+            {
+                foreach (var file in filesInDirectory)
+                {
+                    for (int i = 0; i < depth; i++)
+                    {
+                        Console.Write("\t");
+                    }
+                    Console.Write($"File: {file.name}, Size: {file.fileSize}");
+                    Console.WriteLine();
+                }
+            }
+
+            if (subDirectories != null)
+            {
+                foreach (var dir in subDirectories)
+                {
+                    for (int i = 0; i < depth; i++)
+                    {
+                        Console.Write("\t");
+                    }
+                        dir.CountDirSize();
+                        Console.Write($"Dir: {dir.name}, Size: {dir.dirSize}, Depth: {dir.depth}");
+                        Console.WriteLine();
+                        dir.PrintDir();
+                }
+            }
         }
     }
 
+    
+    
 
     class Program
     {
         static void Main()
         {
             var lines = Common.ParseFile("input.txt");
-            List<string> commands = new List<string>();
-            List<string> dirs = new List<string>();
-            List<string> files = new List<string>();
-
-            Directory root = new Directory("/");
-
+            // List<string> commands = new List<string>();
+            // List<string> dirs = new List<string>();
+            // List<string> files = new List<string>();
+            
+            Directory root = new Directory("/", 0);
+            Directory currentDirectory = root;
             foreach (var item in lines)
             {
             
-             //   List<string> commands = new List<string>();
-              //  List<string> commands = new List<string>();
-              //var selectedDir = 
                 var args = item.Split(" ");
                 switch (args[0])
                 {
                     case "$":
-                        // commands.Add(item);
-
                         switch (args[1])
                         {
                             case "cd":
-                                Console.WriteLine("selected Dir: " + args[2]);
+              //                  Console.WriteLine("selected Dir: " + args[2]);
+              
+                                if (args[2] != "/" && args[2] != "..") 
+                                {
+                                    var nextDir = new Directory(args[2], currentDirectory.depth+1);
+                                    currentDirectory.subDirectories.Add(nextDir);
+                                    nextDir.parent = currentDirectory;
+                                    currentDirectory = nextDir;
+                                }
+
+                                if (args[2] == "..")
+                                {
+                                    var prevDir = currentDirectory.parent;
+                                    currentDirectory = prevDir;
+                                }
                                 break;
                             case "ls":
-                                Console.WriteLine("ls");
+                        //        Console.WriteLine("ls");
                                 break;
                             default:
                                 break;
@@ -64,28 +137,26 @@ namespace Day7
 
                         break;
                     case "dir":
-                        dirs.Add(item);
+                     //   dirs.Add(item);
                         break;
                         
                     default:
                         File f = new File(args[1], int.Parse(args[0]));
-                        Console.WriteLine($"File: {f.name}, Size: {f.fileSize}");
+                     //   Console.WriteLine($"File: {f.name}, Size: {f.fileSize}");
+                        currentDirectory.filesInDirectory.Add(f);
                         break;
                 }
             }
-            //foreach (var item in commands)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //foreach (var item in dirs)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //foreach (var item in files)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
+            
+            root.PrintDir();
+            root.CountDirSize();
+            Console.WriteLine($"\nTotalSize: {root.CountTotalSize()}");
+            Console.WriteLine(lines);
+            
+            
+            
+            
+            
         }
     }
 }

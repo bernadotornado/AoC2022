e2e5584f55c8adf42f772c3e7702aef85cb7f2cd
ADMIN\bszha <sze19728@spengergasse.at>
2022-12-09 21:00:00 +0100

    new approach for part 2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using AoC2022;
 
 namespace Day9
 {
     class Program
     {
+
+        class Knot
+        {
+            public Pos pos = new Pos(0,0);
+            public Knot next;
+            private static Knot head;
+
+            public Knot(bool isHead, int length)
+            {
+                if (isHead)
+                    head = this;
+                if (length > 1)
+                    next = new Knot(false, length - 1);
+            }
+            public void Update()
+            {
+                
+                
+                
+                
+                if (next != null)
+                    next.Update();
+            }
+            
+            
+        }
         struct Pos
         {
             public int x;
@@ -18,6 +45,10 @@ namespace Day9
         }
         static void Main(string[] args)
         {
+
+            Knot _head = new Knot(true, 10);
+            
+            
             var lines = Common.ParseFile(@"input.txt");
             Pos head = new Pos(0, 0);
             Pos tail = new Pos(0, 0);

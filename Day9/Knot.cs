6c655c7d22a3821df376003a7783b580598b574e
ADMIN\bszha <sze19728@spengergasse.at>
2022-12-10 13:17:57 +0100

    Day 9 Part 2 Completed

---
+++ Day9/Knot.cs
@@ -0,0 +1,74 @@
+﻿using  System;
+using  System.Collections.Generic;
+
+namespace Day9
+{
+    public class Knot
+    {
+            public struct Pos
+            {
+                public int x;
+                public int y;
+                public Pos(int x, int y)
+                {
+                    this.x = x;
+                    this.y = y;
+                }
+            }
+            
+            public Pos pos = new Pos(0,0);
+            public Knot next;
+            public Knot parent;
+            private static Knot head;
+            private static Knot tail;
+
+            public Knot(bool isHead, int length)
+            {
+                if (isHead)
+                    head = this;
+                if (length <= 1)
+                    tail = this;
+                else
+                {
+                    next = new Knot(false, length - 1);
+                    next.parent = this;
+                }
+            }
+            public void Update(string c, List<Pos> visited)
+            {
+                if (head == this)
+                    MoveHead(c);
+                else
+                    FollowKnot(visited);
+                if(this != tail) 
+                    next.Update(c, visited);
+            }
+
+            private void MoveHead(string c)
+            {
+                switch (c)
+                {
+                    case "R": pos.x++;  break;
+                    case "L": pos.x--;  break;
+                    case "U": pos.y++;  break;
+                    case "D": pos.y--;  break;
+                }
+            }
+
+            private void FollowKnot(List<Pos> visited)
+            {
+                int stepX = parent.pos.x - pos.x;
+                int stepY = parent.pos.y - pos.y;
+
+                if (Math.Abs(stepX) > 1 || Math.Abs(stepY) > 1)
+                {
+                    pos.x += Math.Sign(stepX);
+                    pos.y += Math.Sign(stepY);
+                }
+
+                if (this == tail)
+                    if (!visited.Contains(pos))
+                        visited.Add(tail.pos);
+            }
+    }
+}
--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,100 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using AoC2022;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace Day9
 {
     class Program
     {
-        private static int iter = 0;
-
-        class Knot
-        {
-            public Pos pos = new Pos(0,0);
-            public Knot next;
-            public Knot parent;
-            public Pos last;
-            private static Knot head;
-            private static Knot tail;
-
-            public Knot(bool isHead, int length)
-            {
-                if (isHead)
-                    head = this;
-                if(!(length> 1))
-                    tail = this;
-                else
-                {
-                    next = new Knot(false, length - 1);
-                    next.parent = this;
-                }
-            }
-            public void Update(string c, List<Pos> visited)
-            {
-                
-                    
-                    
-                        last = pos;
-                        if (head == this)
-                        {
-                            switch (c)
-                            {
-                                case "R":
-                                    pos.x++; break;
-                                case "L": 
-                                    pos.x--; break;
-                                case "U":
-                                    pos.y++; break;
-                                case "D":
-                                    pos.y--; break;
-                            }
-                            next.Update(c, visited);
-                        }
-                        else
-                        {
-                            
-                            if (Math.Abs(parent.pos.x - pos.x) > 1 || Math.Abs(parent.pos.y - pos.y) > 1)
-                            {
-                                pos = parent.last;
-                            }
-                            if (this == tail)
-                            {
-                                if (!visited.Contains(pos))
-                                {
-                                    visited.Add(tail.pos);
-                                    
-                                }
-                            }
-                           
-                            if(this != tail) next.Update(c, visited);
-                        }
-                    
-                        // TODO: FIX THIS MESS
-                
-
-                
-            }
-            
-            
-        }
-        struct Pos
-        {
-            public int x;
-            public int y;
-            public Pos(int x, int y)
-            {
-                this.x = x;
-                this.y = y;
-            }
-        }
         static void Main(string[] args)
         {
-            List<Pos> SimulateRope(List<string> list, int ropeLength)
+            List<Knot.Pos> SimulateRope(List<string> list, int ropeLength)
             {
                 Knot head = new Knot(true, ropeLength);
-                List<Pos> visited = new List<Pos>();
+                List<Knot.Pos> visited = new List<Knot.Pos>();
                 foreach (var line in list)
                 {
                     var arg = line.Split(" ");
@@ -105,7 +22,7 @@ namespace Day9
                 }
                 return visited;
             }
-            var lines = Common.ParseFile(@"test.txt");
+            var lines = Common.ParseFile(@"input.txt");
             Console.WriteLine($"Part 1 Score: {SimulateRope(lines, 2).Count}\n" +
                               $"Part 2 Score: {SimulateRope(lines, 10).Count}\n");
            
